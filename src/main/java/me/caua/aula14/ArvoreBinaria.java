22ef6169b49e805b4743b04d1f8c75828a2199b9
tanz <cauamelo2345@gmail.com>
2024-05-16 00:15:17 -0300

    refactor get altura

---
+++ src/main/java/me/caua/aula14/ArvoreBinaria.java
@@ -0,0 +1,169 @@
+package me.caua.aula14;
+
+public class ArvoreBinaria {
+
+    private No noRaiz;
+
+    public ArvoreBinaria() {
+        this.noRaiz = null;
+    }
+
+    public void inserirNo(No noAtual, int elemento) {
+        if (noAtual == null) noAtual = noRaiz;
+        if (noRaiz == null) {
+            this.noRaiz = new No(elemento);
+        } else if (elemento < noAtual.getElemento()) {
+
+            if (noAtual.getEsquerdo() == null) {
+                noAtual.setEsquerdo(new No(elemento));
+            } else {
+                noAtual = noAtual.getEsquerdo();
+                inserirNo(noAtual, elemento);
+            }
+
+        } else {
+
+            if (noAtual.getDireito() == null) {
+                noAtual.setDireito(new No(elemento));
+            } else {
+                noAtual = noAtual.getDireito();
+                inserirNo(noAtual, elemento);
+            }
+
+        }
+    }
+
+    public Integer buscaPreOrdem(No noAtual, int elementoBuscado) {
+        if (noAtual == null) return null;
+
+        System.out.println(noAtual.getElemento());
+
+        if (noAtual.getElemento() == elementoBuscado) {
+            return noAtual.getElemento(); // retornar o elementro encontrado
+        }
+
+        Integer noEsquerdo = buscaPreOrdem(noAtual.getEsquerdo(), elementoBuscado);
+        if (noEsquerdo != null) {
+            return noEsquerdo;
+        }
+
+        Integer noDireito = buscaPreOrdem(noAtual.getDireito(), elementoBuscado);
+        if (noDireito != null) {
+            return noDireito;
+        }
+
+        return null; // elemento não encontrado, fim da lista
+    }
+
+
+    public Integer buscaEmOrdem(No noAtual, int elementoBuscado) {
+        if (noAtual == null) return null;
+
+        Integer noEsquerdo = buscaEmOrdem(noAtual.getEsquerdo(), elementoBuscado);
+        if (noEsquerdo != null) {
+            return noEsquerdo;
+        }
+
+        System.out.println(noAtual.getElemento());
+
+        if (noAtual.getElemento() == elementoBuscado) {
+            return noAtual.getElemento();
+        }
+
+        Integer noDireito = buscaEmOrdem(noAtual.getDireito(), elementoBuscado);
+        if (noDireito != null) {
+            return noDireito;
+        }
+
+        return null; // elemento não encontrado, fim da lista
+    }
+
+    public Integer buscaPosOrdem(No noAtual, int elementoBuscado) {
+        if (noAtual == null) return null;
+
+        Integer noEsquerdo = buscaPosOrdem(noAtual.getEsquerdo(), elementoBuscado);
+        Integer noDireito = buscaPosOrdem(noAtual.getDireito(), elementoBuscado);
+
+        System.out.println(noAtual.getElemento());
+
+        if (noAtual.getElemento() == elementoBuscado) {
+            return noAtual.getElemento();
+        }
+
+        if (noEsquerdo != null) {
+            return noEsquerdo;
+        }
+
+        if (noDireito != null) {
+            return noDireito;
+        }
+
+        return null; // elemento não encontrado, fim da lista
+    }
+
+
+    public Integer buscar(No noAtual, int elemento) {
+        if (noAtual == null) return null;
+
+        if (elemento < noAtual.getElemento()) {
+            return buscar(noAtual.getEsquerdo(), elemento);
+        } else if (noAtual.getElemento() == elemento) {
+            return noAtual.getElemento(); // elemento encontrado
+        } else {
+            return buscar(noAtual.getDireito(), elemento);
+        }
+    }
+
+    public No remover(No raiz, int elemento) {
+        if (raiz == null) {
+            return null; // elemento não encontrado
+        }
+
+        // busca recursiva para encontrar o nó a ser removido
+        if (elemento < raiz.getElemento()) {
+            raiz.setEsquerdo(remover(raiz.getEsquerdo(), elemento));
+        } else if (elemento > raiz.getElemento()) {
+            raiz.setDireito(remover(raiz.getDireito(), elemento));
+        } else {
+            // Nó a ser removido encontrado
+
+            // Nó folha (sem filhos)
+            if (raiz.getEsquerdo() == null && raiz.getDireito() == null) {
+                return null; // Simplesmente remove o nó folha
+            }
+
+            if (raiz.getEsquerdo() != null) {
+                raiz.setElemento(raiz.getEsquerdo().getElemento()); // Substitui o elemento pelo do filho esquerdo
+                raiz.setEsquerdo(remover(raiz.getEsquerdo(), raiz.getElemento())); // Remove o filho esquerdo recursivamente
+            } else {
+                raiz.setElemento(raiz.getDireito().getElemento()); // Substitui o elemento pelo do filho direito
+                raiz.setDireito(remover(raiz.getDireito(), raiz.getElemento())); // Remove o filho direito recursivamente
+            }
+        }
+
+        return raiz; // Retorna a raiz atualizada
+    }
+
+    private int getAltura(No raiz) {
+        if (raiz == null)
+            return 0;
+
+        int alturaEsquerda = getAltura(raiz.getEsquerdo());
+        int alturaDireita = getAltura(raiz.getDireito());
+
+        return Math.max(alturaEsquerda, alturaDireita) + 1;
+    }
+
+    private boolean hasNoEsquerdo(No atual) {
+        return atual.getEsquerdo() != null;
+    }
+
+    private boolean hasNoDireito(No atual) {
+        return atual.getDireito() != null;
+    }
+
+    public No getRaiz() {
+        return noRaiz;
+    }
+
+}
---
+++ src/main/java/me/caua/aula14/No.java
@@ -0,0 +1,47 @@
+package me.caua.aula14;
+
+public class No {
+
+    private Integer elemento;
+    private No esquerdo;
+    private No direito;
+
+    public No(Integer elemento) {
+        this.elemento = elemento;
+        this.esquerdo = null;
+        this.direito = null;
+    }
+
+    public Integer getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Integer elemento) {
+        this.elemento = elemento;
+    }
+
+    public No getEsquerdo() {
+        return esquerdo;
+    }
+
+    public void setEsquerdo(No esquerdo) {
+        this.esquerdo = esquerdo;
+    }
+
+    public No getDireito() {
+        return direito;
+    }
+
+    public void setDireito(No direito) {
+        this.direito = direito;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "elemento=" + elemento +
+                ", esquerdo=" + esquerdo +
+                ", direito=" + direito +
+                '}';
+    }
+}
